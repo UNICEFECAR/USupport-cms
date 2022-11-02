e520fd63140de2e03f86a29be107a0264215125c
Sebastian Galustyan <sebastian@7digit.io>
2022-11-02 00:44:32 +0000

    Create: new custom route and controller for incrementin articles read_count

--- src/api/article/controllers/article.js
+++ src/api/article/controllers/article.js
@@ -1,9 +1,32 @@
-'use strict';
+"use strict";
 
 /**
  * article controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::article.article');
+module.exports = createCoreController("api::article.article", ({ strapi }) => ({
+  async addReadCount(ctx) {
+    try {
+      const { id } = ctx.params;
+
+      const result = await strapi.db
+        .query("api::article.article")
+        .findOne({ select: ["read_count"], where: { id: id } });
+
+      const resultAfterUpdate = await strapi.db
+        .query("api::article.article")
+        .update({
+          where: { id: id },
+          data: { read_count: parseInt(result.read_count) + 1 },
+        });
+
+      ctx.body = resultAfterUpdate;
+    } catch (err) {
+      console.log(err);
+      ctx.status = 500;
+      ctx.body = { error: err.message };
+    }
+  },
+}));
