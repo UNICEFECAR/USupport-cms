cae75960aa0044834c44e9c9bece072405261547
Sebastian Galustyan <sebastian@7digit.io>
2022-11-06 04:42:27 +0000

    Create: sos-center collection type

---
+++ src/api/sos-center/controllers/sos-center.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * sos-center controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::sos-center.sos-center');
