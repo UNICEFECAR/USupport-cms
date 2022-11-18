224b9bbfd723313c6ee570dc32119fc1e2310b92
Sebastian Galustyan <sebastian@7digit.io>
2022-11-18 02:25:42 +0000

    Add: to privacy policy customFind controller accessible from "/policy-cookies/find"

--- src/api/privacy-policy/controllers/privacy-policy.js
+++ src/api/privacy-policy/controllers/privacy-policy.js
@@ -1,9 +1,40 @@
-'use strict';
+"use strict";
 
 /**
  * privacy-policy controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::privacy-policy.privacy-policy');
+module.exports = createCoreController(
+  "api::privacy-policy.privacy-policy",
+  ({ strapi }) => ({
+    async customFind(ctx) {
+      /**
+       * #route   GET /privacy-policies/find
+       * #desc    Get privacy policies
+       */
+      try {
+        const { query } = ctx;
+
+        const privacyPolicies = await strapi.db
+          .query("api::privacy-policy.privacy-policy")
+          .findMany({ where: { locale: query.locale } });
+
+        let result = null;
+        for (let i = 0; i < privacyPolicies.length; i++) {
+          const currentData = privacyPolicies[i];
+          if (currentData.country === query.country) {
+            result = currentData[query.platform];
+          }
+        }
+
+        ctx.body = result;
+      } catch (err) {
+        console.log(err);
+        ctx.status = 500;
+        ctx.body = { error: err.message };
+      }
+    },
+  })
+);
