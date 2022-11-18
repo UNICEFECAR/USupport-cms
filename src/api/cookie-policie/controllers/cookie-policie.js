9a40bd53d9985428e91541d051e51b51bb0858dd
Sebastian Galustyan <sebastian@7digit.io>
2022-11-18 02:20:27 +0000

    Refactor: remove cookie-policy colection type

--- src/api/cookie-policie/controllers/cookie-policie.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * cookie-policie controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::cookie-policie.cookie-policie');
--- src/api/cookie-policie/services/cookie-policie.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * cookie-policie service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::cookie-policie.cookie-policie');
