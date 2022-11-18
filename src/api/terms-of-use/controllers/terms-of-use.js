7551a46f81d1b3f447ac04ed28d825f85ec8567e
Sebastian Galustyan <sebastian@7digit.io>
2022-11-18 02:26:00 +0000

    Add: to terms of use customFind controller accessible from "/policy-cookies/find"

--- src/api/terms-of-use/controllers/terms-of-use.js
+++ src/api/terms-of-use/controllers/terms-of-use.js
@@ -6,4 +6,35 @@
 
 const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController("api::terms-of-use.terms-of-use");
+module.exports = createCoreController(
+  "api::terms-of-use.terms-of-use",
+  ({ strapi }) => ({
+    async customFind(ctx) {
+      /**
+       * #route   GET /terms-of-uses/find
+       * #desc    Get terms of use
+       */
+      try {
+        const { query } = ctx;
+
+        const termsOfUse = await strapi.db
+          .query("api::terms-of-use.terms-of-use")
+          .findMany({ where: { locale: query.locale } });
+
+        let result = null;
+        for (let i = 0; i < termsOfUse.length; i++) {
+          const currentData = termsOfUse[i];
+          if (currentData.country === query.country) {
+            result = currentData[query.platform];
+          }
+        }
+
+        ctx.body = result;
+      } catch (err) {
+        console.log(err);
+        ctx.status = 500;
+        ctx.body = { error: err.message };
+      }
+    },
+  })
+);
