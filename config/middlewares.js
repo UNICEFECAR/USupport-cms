d2c4aa75bc18cb56d63263f1b0206ae394aef402
Sebastian Galustyan <sebastian@7digit.io>
2022-11-20 22:32:07 +0200

    Add: functionality for aws-s3 bucket media plugin upload

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,6 +1,31 @@
 module.exports = [
   "strapi::errors",
-  "strapi::security",
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          "img-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "dl.airtable.com",
+            "usupport-staging.s3.eu-central-1.amazonaws.com",
+          ],
+          "media-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "dl.airtable.com",
+            "usupport-staging.s3.eu-central-1.amazonaws.com",
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   {
     name: "strapi::cors",
     config: {
