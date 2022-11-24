11a9e17739de8f2745c616285cfab4c659f4dc25
Sebastian Galustyan <sebastian@7digit.io>
2022-11-24 14:28:22 +0000

    Refactor: new staging domain name

--- config/server.js
+++ config/server.js
@@ -1,8 +1,8 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  url: 'https://staging.7digit.io/cms',
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
+  url: "https://staging.usupport.online/cms",
   app: {
-    keys: env.array('APP_KEYS'),
+    keys: env.array("APP_KEYS"),
   },
 });
