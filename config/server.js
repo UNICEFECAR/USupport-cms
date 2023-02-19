4a7c9f5288285021908073ca10cf5b759ee35596
vasilen-7DIGIT <vasilen@7digit.io>
2023-02-19 00:18:28 +0000

    Refactor: production setup

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
-  url: "https://staging.usupport.online/cms",
+  url: "https://usupport.online/cms",
   app: {
     keys: env.array("APP_KEYS"),
   },
