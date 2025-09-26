2d84cdb67dff575a93efa3db30899c400f5a7307
7Digit-Main <it-support@7digit.io>
2025-09-26 13:47:24 +0300

    Create: mood tracker recommendation

---
+++ src/api/mood-tracker-recomendation/routes/mood-tracker-recomendation.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * mood-tracker-recomendation router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::mood-tracker-recomendation.mood-tracker-recomendation');
